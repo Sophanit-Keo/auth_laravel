9b09c58c265369f0f325c27420dcb55c5457248c
Sophanit-Keo <sophanitkeo1803@gmail.com>
2026-06-20 15:37:04 +0700

    testing route

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,11 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('category', [CategoryController::class, 'store']);
 });
+Route::get('/test-db', function () {
+    try {
+        $dbName = \Illuminate\Support\Facades\DB::connection()->getDatabaseName();
+        return "✅ Connected to: <strong>{$dbName}</strong>";
+    } catch (\Exception $e) {
+        return "❌ Error: " . $e->getMessage();
+    }
+});

1cedd2cc4780bdb598121cd1662a662bbe7ed0ea
Sophanit-Keo <sophanitkeo1803@gmail.com>
2026-06-20 14:46:34 +0700

    updated config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
